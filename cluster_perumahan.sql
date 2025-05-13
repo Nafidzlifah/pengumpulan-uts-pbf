ce4e29883e3b7859cbf2d2c463b428eef8469490
Nafidzlifah <if23.nafidzalifah@mhs.ubpkarawang.ac.id>
2025-05-13 14:12:38 +0700

    Add files via upload

---
+++ cluster_perumahan.sql
@@ -0,0 +1,302 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 13, 2025 at 07:09 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cluster_perumahan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ketua_rt`
+--
+
+CREATE TABLE `ketua_rt` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_hp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `periode_mulai` year NOT NULL,
+  `periode_selesai` year NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2025_04_26_074458_create_tamu_pendatang_table', 1),
+(2, '2025_04_26_074528_create_warga_lokal_table', 1),
+(3, '2025_04_26_074540_create_satpam_table', 1),
+(4, '2025_04_26_074547_create_ketua_rt_table', 1),
+(5, '2025_04_26_075705_create_pengajuan_surat_table', 1),
+(6, '2025_04_26_075713_create_pengaduan_table', 1),
+(7, '2025_05_10_122642_create_sessions_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaduan`
+--
+
+CREATE TABLE `pengaduan` (
+  `id` bigint UNSIGNED NOT NULL,
+  `warga_id` bigint UNSIGNED NOT NULL,
+  `isi_pengaduan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('menunggu','diproses','selesai') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'menunggu',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengajuan_surat`
+--
+
+CREATE TABLE `pengajuan_surat` (
+  `id` bigint UNSIGNED NOT NULL,
+  `warga_id` bigint UNSIGNED NOT NULL,
+  `jenis_surat` enum('nikah','domisili','ktp','kk','lainnya') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci,
+  `status` enum('menunggu','disetujui','ditolak') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'menunggu',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satpam`
+--
+
+CREATE TABLE `satpam` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_hp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shift` enum('pagi','siang','malam') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('EbEfDvg578zCDUWLrMP9jxU5J9fcpOc5SvdKfiLU', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoieGdDWmFhNE02ZnhPQ0hianJrNmVGeHpiOFZ3eFlSdzZlMjRwZm1rRyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1746884320),
+('PgdyMAh8QIsK7vMVrxzK9al5ZBJ9E6ycOFYGeypJ', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoicjh1MUNFUnAxa0todTlqNllMbkI5OFdydjFvS1Y3TEZhc0FicTU0UiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1747052281);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tamu_pendatang`
+--
+
+CREATE TABLE `tamu_pendatang` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_identitas` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat_asal` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ke_rumah` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alasan_kunjungan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `waktu_masuk` datetime NOT NULL,
+  `waktu_keluar` datetime DEFAULT NULL,
+  `status` enum('masuk','keluar') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'masuk',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warga`
+--
+
+CREATE TABLE `warga` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nik` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kk` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat_rumah` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_rumah` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_hp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ketua_rt`
+--
+ALTER TABLE `ketua_rt`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ketua_rt_email_unique` (`email`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pengaduan_warga_id_foreign` (`warga_id`);
+
+--
+-- Indexes for table `pengajuan_surat`
+--
+ALTER TABLE `pengajuan_surat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pengajuan_surat_warga_id_foreign` (`warga_id`);
+
+--
+-- Indexes for table `satpam`
+--
+ALTER TABLE `satpam`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `satpam_email_unique` (`email`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `tamu_pendatang`
+--
+ALTER TABLE `tamu_pendatang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `warga`
+--
+ALTER TABLE `warga`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `warga_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ketua_rt`
+--
+ALTER TABLE `ketua_rt`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengajuan_surat`
+--
+ALTER TABLE `pengajuan_surat`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `satpam`
+--
+ALTER TABLE `satpam`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tamu_pendatang`
+--
+ALTER TABLE `tamu_pendatang`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `warga`
+--
+ALTER TABLE `warga`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD CONSTRAINT `pengaduan_warga_id_foreign` FOREIGN KEY (`warga_id`) REFERENCES `warga` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `pengajuan_surat`
+--
+ALTER TABLE `pengajuan_surat`
+  ADD CONSTRAINT `pengajuan_surat_warga_id_foreign` FOREIGN KEY (`warga_id`) REFERENCES `warga` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
